9ad671710f927e1c935f3f2789a65e540d483523
鸡毛 <shihao.gu@perfma.com>
2019-01-08 11:54:25 +0800

    开方导数验证

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ a = np.array([3.0, 3.0, 2.0, 4.9, 100.2, -8.9]);
 def gradient ():
     d = 1e-5;
     grad = np.zeros(a.size);
-    # func = lambda : np.std(a, ddof = 1);
-    func = lambda : np.mean(a);
+    func = lambda : np.std(a, ddof = 1);
+    # func = lambda : np.mean(a);
     for index, value in enumerate(a):
         bak = value;
         a[index] -= d;
@@ -25,6 +25,24 @@ def gradient ():
 grad = gradient();
 
 print(grad);
+
+# std = np.std(a, ddof = 1);
+# avg = np.mean(a);
+# k = std ** 2;
+# t = 0.5 * np.power(k, -0.5);
+# print(a.size);
+# print(t * (a - avg) / a.size);
+
+d = 1e-5;
+a = 10;
+leftv = np.sqrt(a - d);
+rightv = np.sqrt(a + d);
+rst = (rightv - leftv) / (d * 2);
+print(rst);
+rst2 = 0.5 * np.power(a, -0.5);
+print(rst2);
+
+
 
 
 
